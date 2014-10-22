0a45b414028274afdf7acd0b39eb0c8e536cfb96
HappyShadow <happyshadow@icloud.com>
2014-10-22 15:56:37 +0800

    create a test app

---
+++ FirstGame/FirstGame/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  FirstGame
+//
+
+//  Copyright (c) 2014 Moon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
